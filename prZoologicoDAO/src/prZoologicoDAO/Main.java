a0207a42a5b59e5ea22c6b3364db34c9f76e4763
JoaquinMF <kino_1992@hotmail.com>
2022-10-14 14:33:46 +0200

    mensaje

---
+++ prZoologicoDAO/src/prZoologicoDAO/Main.java
@@ -0,0 +1,21 @@
+package prZoologicoDAO;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Animal a = new Animal("Ardilla","Bosque",0.1);
+
+		AnimalDAO.insertAnimal(a);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}

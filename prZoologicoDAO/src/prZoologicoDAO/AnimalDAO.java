3e2883f00c3ebd81760d18fba82c46ba39974b71
JoaquinMF <kino_1992@hotmail.com>
2022-10-21 13:33:49 +0200

    findAnimal

--- prZoologicoDAO/src/prZoologicoDAO/AnimalDAO.java
+++ prZoologicoDAO/src/prZoologicoDAO/AnimalDAO.java
@@ -2,6 +2,7 @@ package prZoologicoDAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -9,8 +10,56 @@ public abstract class AnimalDAO {
 	
 	private static Connection connection;
 	
+	public static Animal findById(int id) {
+		
+		connection = openConnection();
+		
+		String query = "select * from animales where id = ?";
+		Animal animal=null;
+		
+		try {
+			PreparedStatement ps= connection.prepareStatement(query);
+			ps.setInt(1, id);
+			ResultSet rs= ps.executeQuery();
+			
+			while(rs.next()) {
+				
+				animal = new Animal(rs.getInt("id"),rs.getString("nombre"),rs.getString("habitat"),rs.getDouble("peso_aproximado"));
+				
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		closeConnection();
+		
+		return animal;
+		
+	}
 	
-	public static void deleteAnimal() {
+	public static void deleteAnimalByNombre (String nombre) {
+		
+		connection =openConnection();
+		
+		String query = "delete from animales where nombre = ?";
+		
+		try {
+			PreparedStatement ps = connection.prepareStatement(query);
+			ps.setString(1, nombre);
+			ps.executeUpdate();
+			
+			
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		closeConnection();
+	
+	}
+		
+	
+	public static void deleteAllAnimales() {
 		
 		connection =openConnection();
 		
--- prZoologicoDAO/src/prZoologicoDAO/Main.java
+++ prZoologicoDAO/src/prZoologicoDAO/Main.java
@@ -5,15 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		
 		//Animal a = new Animal("Ardilla","Bosque",0.1);
-
 		//AnimalDAO.insertAnimal(a);
+		//AnimalDAO.deleteAnimalByNombre("Ardilla");
 		
 		
-		
-		AnimalDAO.deleteAnimal();
-		
-		
-		
+		Animal b = AnimalDAO.findById(3);
+		System.out.println(b);
 		
 		
 	}

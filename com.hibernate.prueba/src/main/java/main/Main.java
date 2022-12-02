220e6f3c9dba564c8cc42e19c5ed5a94222a9605
JoaquinMF <kino_1992@hotmail.com>
2022-12-02 14:27:20 +0100

    mensaje

---
+++ com.hibernate.prueba/src/main/java/main/Main.java
@@ -0,0 +1,51 @@
+package main;
+
+import java.math.BigDecimal;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+
+import pojo.Animal;
+import util.HibernateUtil;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Session session=HibernateUtil.getSessionFactory().openSession();
+		
+		//Animal a1=new Animal("Koala","Jungla",new BigDecimal(2));
+		
+		//session.save(a1);
+		
+		/*int id=1;
+		
+		Animal a=(Animal) session.get(Animal.class, id);
+		
+		System.out.println(a.getNombre());*/
+		
+		//List animales=session.createQuery("FROM Animal").getResultList();
+		
+		String hql="FROM Animal WHERE habitat=:habitat";
+		Query query =session.createQuery(hql);
+		query.setParameter("habitat","Jungla");
+		List animales=query.getResultList();
+		
+		for(Iterator i=animales.iterator();i.hasNext();) {
+			
+			Animal a = (Animal) i.next();
+			System.out.println(a.getNombre());
+			
+		}
+		
+		
+		session.close();
+		
+		
+
+	}
+
+}
